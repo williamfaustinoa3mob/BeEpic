5b325bab3acc4c94d04012668461ce7033e489e3
williamfaustinoa3mob <william.faustino@a3mob.com.br>
2016-12-05 15:50:03 -0200

    Initial commit

---
+++ app/src/main/java/defora/beepic/MainActivity.java
@@ -0,0 +1,93 @@
+package defora.beepic;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_Play;
+    private Button btn_Pause;
+    private Button btn_Stop;
+    private Musica musicaAtual = new Musica();
+
+    // Volume
+    private SeekBar skb_Volume;
+    private AudioManager audioManager;
+    private int maxVolume;
+    private int curVolume;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_Play = (Button) findViewById(R.id.btn_play) ;
+        btn_Play.setOnClickListener(this);
+
+        btn_Pause = (Button) findViewById(R.id.btn_pause) ;
+        btn_Pause.setOnClickListener(this);
+
+        btn_Stop = (Button) findViewById(R.id.btn_stop) ;
+        btn_Stop.setOnClickListener(this);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        AlterarVolume();
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+
+            case R.id.btn_play:
+                Log.i("MainLog","PLAY");
+                musicaAtual.Play(this,R.raw.taverna);
+                break;
+
+            case R.id.btn_pause:
+                Log.i("MainLog","PAUSE");
+                musicaAtual.Pause();
+                break;
+
+            case R.id.btn_stop:
+                Log.i("MainLog","STOP");
+                musicaAtual.Stop();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void AlterarVolume() {
+
+        skb_Volume = (SeekBar) findViewById(R.id.skb_volume);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        skb_Volume.setMax(maxVolume);
+        skb_Volume.setProgress(curVolume);
+        skb_Volume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onStopTrackingTouch(SeekBar arg0) {
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar arg0) {
+
+                    }
+
+                    @Override
+                    public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, arg1, 0);
+                    }
+                });
+    }
+
+}
---
+++ app/src/main/java/defora/beepic/Musica.java
@@ -0,0 +1,41 @@
+package defora.beepic;
+
+import android.media.MediaPlayer;
+import android.content.Context;
+import android.util.Log;
+
+public class Musica {
+
+    private MediaPlayer musicaAtual;
+
+    public void Play(Context c, int nomeMusica)
+    {
+        Log.i("MusicaLog","MediaPlayer.create");
+        // No futuro, o MediaPlayer.create terá que ser movido para o método de seleção de músicas
+        // Deixar esse código no método Play() faz com que o objeto seja recriado todas as vezes, atrapalhando na funcionalidade do método Pause()
+
+        if (musicaAtual == null) { // Este if serve para auxiliar o método Pause. Quando o MediaPlayer.create for movido do Play(), esse if deverá ser apagado
+            musicaAtual = MediaPlayer.create(c, nomeMusica);
+        }
+
+        Log.i("MusicaLog","musicaAtual.start()");
+        musicaAtual.start();
+    }
+
+    public void Pause()
+    {
+        Log.i("MusicaLog","musicaAtual.pause()");
+        if (musicaAtual != null) { // Este if serve para evitar que o app crash devido ao musicaAtual estár nulo na inicialização do programa
+            musicaAtual.pause();
+        }
+    }
+
+    public void Stop()
+    {
+        Log.i("MusicaLog","musicaAtual.stop()");
+        if (musicaAtual != null) { // Este if serve para evitar que o app crash devido ao musicaAtual estár nulo na inicialização do programa
+            musicaAtual.stop();
+            musicaAtual = null; // Estou setando nulo para que force a criação do musicaAtual no método Play(), essa linha deve ser apagada depois que o MediaPlayer.create for movido para o método de seleção de músicas
+        }
+    }
+}
